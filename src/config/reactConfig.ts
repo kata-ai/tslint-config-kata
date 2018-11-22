2305efbe59ef1b00993be59cf07ae19be1f5091f
Resi Respati <resir014@gmail.com>
2018-11-22 19:21:15 +0700

    fix(react): allow PascalCased function names for react components

--- src/config/reactConfig.ts
+++ src/config/reactConfig.ts
@@ -19,8 +19,18 @@ export default {
     'jsx-wrap-multiline': true,
 
     // Overrides from base config
+    'function-name': [
+      true,
+      {
+        'function-regex': /^[\w\d]+$/,
+        'method-regex': /^[\w\d]+$/,
+        'private-method-regex': /^[\w\d]+$/,
+        'protected-method-regex': /^[\w\d]+$/,
+        'static-method-regex': /^[\w\d]+$/
+      } // 23.1
+    ],
     'interface-name': [true, 'never-prefix'],
-    'variable-name': [true, 'ban-keywords'], // 23.2 exception
-    'ter-indent': [true, 2, { SwitchCase: 1 }] // 19.1 exception
+    'ter-indent': [true, 2, { SwitchCase: 1 }], // 19.1 exception
+    'variable-name': [true, 'ban-keywords'] // 23.2 exception
   }
 };
